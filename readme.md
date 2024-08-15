febec7ce5e8694244a5c59d6cf670d9f4289eb69
Chinmay Chaudhary <chinmay.chaudhary@sprinklr.com>
2024-08-15 16:11:16 +0530

    minor wordings

--- readme.md
+++ readme.md
@@ -2,4 +2,3 @@
 2. Click on the first task to open a card view. Then, click on refresh. You will notice that the data in the first row of the table updates, but it does not update in the card view. Fix this issue.
 3. Editing a task in the card view should also update the task in the table view.
 4. When a card view for the first task is open, clicking on the second task in the table view should update the card view to show the second task. Currently, it still shows the first task. Fix this issue.
-5. Allow multiple card views to be open simultaneously by clicking on different tasks in the table view. Currently, only one card view is rendered at any given time.
--- src/__tests__/sampleTest.spec.js
+++ src/__tests__/sampleTest.spec.js
@@ -70,7 +70,7 @@ describe("1. The refresh icon should spin only when the refresh is in progress."
   });
 });
 
-describe("2. Click on the first task to open a card view. Then, click on refresh. You will notice that the data in the first row of the table updates, but it does not update in the card view. Fix this issue.", () => {
+describe("2. When data is refreshed in the table, it should be reflected in the card view too.", () => {
   let tasks = [
     {
       id: 1,
@@ -161,7 +161,7 @@ describe("3. Editing a task in the card view should also update the task in the 
   });
 });
 
-describe("4. When a card view for the first task is open, clicking on the second task in the table view should update the card view to show the second task. Currently, it still shows the first task. Fix this issue.", () => {
+describe("4. Clicking on a task in the table should update the task card view", () => {
   let tasks = [
     {
       id: 1,
